4951b012dbf677fd276aa49692e0b045ca0b7a43
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-01-26 12:52:13 -0500

    Updates

---
+++ precache-manifest.0466d1a68c20b6e53d398aaba9e6af39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6416de70a9cec7f381bd9b39bc9adab4",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "021f031b017b7d8fa2bb",
+    "url": "/face-recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a35fa763af084d0fadec",
+    "url": "/face-recognition/static/css/main.a5291e69.chunk.css"
+  },
+  {
+    "revision": "021f031b017b7d8fa2bb",
+    "url": "/face-recognition/static/js/2.f46927c5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/face-recognition/static/js/2.f46927c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a35fa763af084d0fadec",
+    "url": "/face-recognition/static/js/main.02fd229a.chunk.js"
+  },
+  {
+    "revision": "c04e1b1832214533dd87",
+    "url": "/face-recognition/static/js/runtime-main.6d82ba93.js"
+  }
+]);
